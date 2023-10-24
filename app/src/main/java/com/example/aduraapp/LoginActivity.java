2e76c0301ea0717fef353e2845a74d48857cc9e8
insan02 <nurulinsan201002@gmail.com>
2023-10-24 09:43:53 +0700

    tambah activity login

---
+++ app/src/main/java/com/example/aduraapp/LoginActivity.java
@@ -0,0 +1,50 @@
+package com.example.aduraapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.aduraapp.databinding.ActivityLoginBinding;
+import com.example.aduraapp.databinding.ActivityMainBinding;
+import com.google.android.material.textfield.TextInputEditText;
+
+public class LoginActivity extends AppCompatActivity {
+
+    TextInputEditText editUsername, editPassword;
+
+    private ActivityLoginBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_login);
+
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        editUsername = findViewById(R.id.editUsername);
+        editPassword = findViewById(R.id.editPassword);
+
+    }
+
+
+    public void onButtonLoginClicked(View view) {
+        //explicit intent
+        String username = binding.editUsername.getText().toString();
+        String password = binding.editPassword.getText().toString();
+
+        if(password.equals("123")){
+            Intent mainIntent = new Intent(this, MainActivity.class);
+            mainIntent.putExtra("USERNAME", username);
+            mainIntent.putExtra("IS_LOGGED_IN", true);
+            startActivity(mainIntent);
+        }else{
+            Toast.makeText(this, "Kombinasi Username dan Password Anda Salah!", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+}
--- app/src/main/java/com/example/aduraapp/MainActivity.java
+++ app/src/main/java/com/example/aduraapp/MainActivity.java
@@ -2,13 +2,44 @@ package com.example.aduraapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.aduraapp.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private boolean isLoggedIn = false;
+//    TextView textGreeting;
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+ //       setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        Intent mainIntent = getIntent();
+        String Username = mainIntent.getStringExtra("USERNAME");
+        isLoggedIn = mainIntent.getBooleanExtra("IS_LOGGED_IN", false);
+
+        if (!isLoggedIn) {
+            Intent loginIntent = new Intent(this, LoginActivity.class);
+            startActivity(loginIntent);
+            finish();
+        }
+
+//        textGreeting = binding.textGreeting;//findViewById(R.id.textGreeting);
+//        textGreeting.setText("Hello, " + Username);
+
+        binding.textGreeting.setText("Hello, " + Username);
     }
+
 }
