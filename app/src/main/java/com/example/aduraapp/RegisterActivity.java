60c82a55f25bf604fb4c8f9bcbb5e9145f81f49c
aanamin <2111521017_al-amin@student.unand.ac.id>
2024-01-06 18:08:48 +0700

    register tambahan

--- app/src/main/java/com/example/aduraapp/RegisterActivity.java
+++ app/src/main/java/com/example/aduraapp/RegisterActivity.java
@@ -17,11 +17,16 @@ import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class RegisterActivity extends AppCompatActivity {
 
-    TextInputEditText email, editPassword, editconfirmPassword;
+    TextInputEditText email, editPassword, editconfirmPassword, name;
 
     private Button register;
 
@@ -38,6 +43,7 @@ public class RegisterActivity extends AppCompatActivity {
         View view = binding.getRoot();
         setContentView(view);
 
+        name = findViewById(R.id.editName);
         email = findViewById(R.id.email);
         editPassword = findViewById(R.id.editPassword);
         editconfirmPassword = findViewById(R.id.editconfirmPassword);
@@ -49,6 +55,7 @@ public class RegisterActivity extends AppCompatActivity {
                 String txt_email = email.getText().toString();
                 String txt_password = editPassword.getText().toString();
                 String txt_confirmPassword = editconfirmPassword.getText().toString();
+                String txt_name = name.getText().toString();
 
                 if(TextUtils.isEmpty(txt_email) || TextUtils.isEmpty(txt_password)){
                     Toast.makeText(RegisterActivity.this, "Empty Credential", Toast.LENGTH_SHORT).show();
@@ -57,24 +64,35 @@ public class RegisterActivity extends AppCompatActivity {
                 } else if (!txt_password.equals(txt_confirmPassword)) {
                     Toast.makeText(RegisterActivity.this, "password and confirm password not same", Toast.LENGTH_SHORT).show();
                 } else {
-                    registerUser(txt_email, txt_password);
+                    registerUser(txt_email, txt_password,txt_name);
                 }
             }
         });
     }
 
-    private void registerUser(String email, String password) {
+    private void registerUser(String email, String password, final String name) {
         Log.d("RegisterActivity", "Attempting to register user...");
 
-        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(RegisterActivity.this,new OnCompleteListener<AuthResult>() {
+        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
                 if (task.isSuccessful()){
-                    Toast.makeText(RegisterActivity.this, "Register Successfull", Toast.LENGTH_SHORT).show();
-                    startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+                    // Registrasi berhasil, tambahkan informasi ke database
+                    String userId = auth.getCurrentUser().getUid();
+                    DatabaseReference usersRef = FirebaseDatabase.getInstance().getReference("users").child(userId);
 
+                    // Tambahkan data pengguna ke Realtime Database
+                    Map<String, Object> userMap = new HashMap<>();
+                    userMap.put("email", email);
+                    userMap.put("name", name);
+
+                    usersRef.setValue(userMap);
+
+                    Toast.makeText(RegisterActivity.this, "Registration Successful", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+                    finish(); // Tutup activity saat ini setelah pendaftaran berhasil
                 } else {
-                    Toast.makeText(RegisterActivity.this, "Register Failed", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(RegisterActivity.this, "Registration Failed", Toast.LENGTH_SHORT).show();
                 }
             }
         });
@@ -86,17 +104,6 @@ public class RegisterActivity extends AppCompatActivity {
     }
 
 
-//    public void onButtonRegisterClicked(View view) {
-//        //explicit intent
-//        String username = binding.editUsername.getText().toString();
-//        String password = binding.editPassword.getText().toString();
-//        String confirmpassword = binding.editconfirmPassword.getText().toString();
-//
-//
-//        Intent loginIntent = new Intent(this, LoginActivity.class);
-//        startActivity(loginIntent);
-//
-//    }
 
 
 }
