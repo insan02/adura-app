a29bd3c0c11ddd14956c4abab5969ac14a08b4fe
insan02 <nurulinsan201002@gmail.com>
2024-01-08 11:23:11 +0700

    tambah admin kebakaran

---
+++ app/src/main/java/com/example/aduraapp/adapters/KebakaranAdminListAdapter.java
@@ -0,0 +1,4 @@
+package com.example.aduraapp.adapters;
+
+public class KebakaranAdminListAdapter {
+}
---
+++ app/src/main/java/com/example/aduraapp/models/KebakaranAdminRiwayat.java
@@ -0,0 +1,4 @@
+package com.example.aduraapp.models;
+
+public class KebakaranAdminRiwayat {
+}
